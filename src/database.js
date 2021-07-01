b782c8c6413f9d2048639f8f9e14c8c8f94b117b
Franfc96 <33126284+TheOddJete@users.noreply.github.com>
2021-07-01 17:18:07 +0200

    Primera version

---
+++ src/database.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'daweb'
+});
+
+pool.getConnection((error, connection) => {
+    if(error){
+        console.error('ERROR AL CONECTAR CON LA BD: ', error.code);
+    }
+
+    if(connection){
+        connection.release();
+        console.log('CONEXIÓN CON ÉXITO A LA BD');
+        return;
+    }
+});
+
+module.exports = pool;
+
---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+// Inicializaciones
+const app = express();
+
+// Configuraciones
+app.set('port', process.env.PORT || 8080);
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+app.use(require('./routes/'));
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Public 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Start
+app.listen(app.get('port'), () => {
+    console.log('Servidor escuchando en el puerto: ', app.get('port'));
+});
+
+//Base de datos
+const pool = require('./database');
