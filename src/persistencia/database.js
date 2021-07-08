e8ee203534d6b0914e726f576104c736ad13d135
YDY <dongyue218@gmail.com>
2021-07-08 23:33:48 +0200

    Modificado algunos estilos, falta revisar los nombre

--- src/persistencia/database.js
+++ src/persistencia/database.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'root',
+    password: 'password',
     database: 'daweb'
 });
 
--- src/public/scripts/scripts.js
+++ src/public/scripts/scripts.js
@@ -1,30 +1,26 @@
-function alerta()
-    {
-    var mensaje;
-    var opcion = confirm("¿Está seguro que desea realizar esta compra?");
-    if (opcion == true) {
-        mensaje = "Has clickado OK";
-	} else {
-	    mensaje = "Has clickado Cancelar";
-	}
-	document.getElementById("ejemplo").innerHTML = mensaje;
+function alertaCompra() {
+  return confirm("¿Está seguro que desea realizar esta compra?");
+}
+
+function alertaCambio() {
+  return confirm("¿Está seguro que desea realizar este cambio?");
 }
 
 //Script de BOOTSTRAP para validar los formularios.
 (function () {
-    'use strict';
-    window.addEventListener('load', function () {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function (form) {
-        form.addEventListener('submit', function (event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
+  'use strict';
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
